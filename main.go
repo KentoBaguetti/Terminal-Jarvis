10c293ff641de44ae6b942dc3a6c65cabb9bc0db
Kentaro Barnes <kentaro.barnes5@gmail.com>
2026-01-27 23:55:39 -0800

    basic cli llm added to input loop

--- main.go
+++ main.go
@@ -6,11 +6,33 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	copilot "github.com/github/copilot-sdk/go"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
+
+	client := copilot.NewClient(nil)
+	if err := client.Start(); err != nil {
+		log.Fatal("Error starting CoPilot client:", err)
+	}
+	defer client.Stop()
+
+	session, err := client.CreateSession(&copilot.SessionConfig{Model: "gpt-4.1", Streaming: true, Tools: []copilot.Tool{}})
+	if err != nil {
+		log.Fatal("Error creating CLI Session:", err)
+	}
+
+	session.On(func(event copilot.SessionEvent) {
+		if event.Type == "assistant.message_delta" {
+			fmt.Print(*event.Data.DeltaContent)
+		}
+		if event.Type == "session.idle" {
+			fmt.Println("\n")
+		}
+	})
 
 	for {
 
@@ -29,7 +51,10 @@ func main() {
 			break
 		}
 
-		fmt.Println(userInput)
+		_, err = session.SendAndWait(copilot.MessageOptions{Prompt: userInput}, 0)
+		if err != nil {
+			log.Fatal("Error sending prompt to CLI:", err)
+		}
 
 	}
 
