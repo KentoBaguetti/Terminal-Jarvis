881011f2fe4b3a6e9d293e61f95244a83b5eda33
Kentaro Barnes <kentaro.barnes5@gmail.com>
2026-01-27 23:40:36 -0800

    set up basic input loop and download correct dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kentobaguetti/terminaljarvis
 
 go 1.25.5
+
+require (
+	github.com/github/copilot-sdk/go v0.1.19 // indirect
+	github.com/google/jsonschema-go v0.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	fmt.Println("Hello")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+
+		fmt.Print("Enter message here (enter 'exit' or click ctrl+c to exit): ")
+
+		userInput, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("Error with user input:", err)
+		}
+
+		userInput = strings.TrimSpace(userInput)
+
+		if userInput == "" {
+			continue
+		} else if userInput == "exit" {
+			break
+		}
+
+		fmt.Println(userInput)
+
+	}
 
 }
